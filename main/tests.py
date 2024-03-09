1fb2169b5d0fea128b5d58605d0d08017854a06e
Satyendra Singh <satyendrasingh@Satyendras-MBP.local>
2024-03-10 02:09:53 +0530

    added model test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import ToDoList
 
 # Create your tests here.
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.list = ToDoList.objects.create(name="fjeo")
+
+    def test_todolist_model(self):
+        l = self.list
+        self.assertTrue(isinstance(l, ToDoList))
+        self.assertEquals(str(l), "fjeo")
